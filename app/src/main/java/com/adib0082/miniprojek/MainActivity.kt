ebfbc892ed8c7d8e412a9835c1a9ddf181e06e8a
Adib Putra Pradipta <emailadibpp@gmail.com>
2026-05-09 15:15:11 +0700

    mainactivity

--- app/src/main/java/com/adib0082/miniprojek/MainActivity.kt
+++ app/src/main/java/com/adib0082/miniprojek/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.adib0082.miniprojek.navigation.SetupNavGraph
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
